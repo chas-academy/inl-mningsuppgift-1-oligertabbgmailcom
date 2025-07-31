736b0f99f1613bf9f06e3dc8d8667aa4d26bebcf
mail@mail.com <mail@mail.com>
2025-08-01 00:02:16 +0200

    lämna in

--- src/script.js
+++ src/script.js
@@ -26,23 +26,23 @@ const contactBook = {
 };
   
   // Ange en grupp
-  let groupToShow;
-  
+  let groupToShow = "familj"
 
+ function showContacts(contactBook, groupToShow) {
+   // Rör ej denna kod
 
+// Loopar igenom alla kontakter och jämför grupp
 
-function showContacts(contactBook, groupToShow) { // Rör ej denna kod
+for (contact of contactBook.contacts) {
+  if (contact.group === groupToShow) {
 
-
-
-    // Loopar igenom alla kontakter och jämför grupp
-   
-    return  // här ska sträng med namn och tekefonnummer visas i form av string interpolation
-
-
+    return `Namn: ${contact.name}, Telefon: ${contact.phone}`;
+    
+     // här ska sträng med namn och tekefonnummer visas i form av string interpolation
+  }
 }
-
-
+ }
+   
 // Rör ej denna kod
 showContacts(contactBook, groupToShow);
 module.exports = { showContacts };
